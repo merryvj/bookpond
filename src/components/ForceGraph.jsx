f293ef929fadf513d500572d391d5e26e182dfc9
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2023-07-28 14:47:56 -0400

    Working version of new ForceGraph

--- src/components/ForceGraph.jsx
+++ src/components/ForceGraph.jsx
@@ -1,16 +1,21 @@
-import React, { useState, useCallback, useEffect, useRef, useContext} from "react";
+import React, {
+  useState,
+  useCallback,
+  useEffect,
+  useRef,
+  useContext,
+} from "react";
 import useGraphState from "../hooks/useGraphState";
 import useBookData from "../hooks/useBookData";
 import * as d3 from "d3";
 import { zoom } from "d3-zoom";
-import { useSpring, animated } from '@react-spring/web'
-import { GraphContext, GraphDispatchContext } from '../hooks/useGraphContext'
-
+import { useSpring, animated } from "@react-spring/web";
+import { GraphContext, GraphDispatchContext } from "../hooks/useGraphContext";
 
 function ForceGraph() {
-  const [focusedNode, setFocusedNode] = useState({ title: "test", id: 0});
-  const {nodeData, linkData} = useBookData(focusedNode);
-  const [state, dispatch] = useGraphState({nodeData, linkData});
+  const [focusedNode, setFocusedNode] = useState({ title: "test", id: 0 });
+  const { nodeData, linkData } = useBookData(focusedNode);
+  const [state, dispatch] = useGraphState({ nodeData, linkData });
 
   //set focused node, which is used to query more data
   const handleClick = useCallback(
@@ -25,38 +30,54 @@ function ForceGraph() {
   //update graph data after clicking on node
   useEffect(() => {
     dispatch({
-      type: 'SelectNode',
+      type: "SelectNode",
       origin: focusedNode,
       nodes: nodeData,
-      links: linkData
-    })
-  }, [nodeData])
+      links: linkData,
+    });
+  }, [nodeData]);
 
   return (
     <GraphContext.Provider value={state}>
-        <GraphDispatchContext.Provider value={dispatch}>
-            <Graph nodeData={state.nodes} linkData={state.links} handleNodeClick={handleClick} />
-        </GraphDispatchContext.Provider>
-
+      <GraphDispatchContext.Provider value={dispatch}>
+        <Graph
+          nodeData={state.nodes}
+          linkData={state.links}
+          handleNodeClick={handleClick}
+        />
+      </GraphDispatchContext.Provider>
     </GraphContext.Provider>
   );
 }
 
 function Graph({ nodeData, linkData, handleNodeClick }) {
-    const svgRef = useRef();
-    const [animatedNodes, setAnimatedNodes] = useState([]);
-    const [animatedLinks, setAnimatedLinks] = useState([]);
-    const graph = useContext(GraphContext);
+  const svgRef = useRef();
+  const [svg, setSvg] = useState();
+  const [animatedNodes, setAnimatedNodes] = useState([]);
+  const [animatedLinks, setAnimatedLinks] = useState([]);
+  const graph = useContext(GraphContext);
+
+  useEffect(() => {
+    if (svgRef.current) {
+      setSvg(d3.select(svgRef.current));
+    }
+  }, []);
 
   // re-create animation every time nodes change
   useEffect(() => {
-    const simulation = d3.forceSimulation(nodeData)
-    .force("charge", d3.forceManyBody().strength(20))
-    .force("collide", d3.forceCollide(50))
-    .force("link", d3.forceLink(linkData)
-     .id(function(d) {
-         return d.id
-     }).strength(2))
+    const simulation = d3
+      .forceSimulation(nodeData)
+      .force("charge", d3.forceManyBody().strength(20))
+      .force("collide", d3.forceCollide(50))
+      .force(
+        "link",
+        d3
+          .forceLink(linkData)
+          .id(function (d) {
+            return d.id;
+          })
+          .strength(2)
+      );
 
     // update state on every frame
     simulation.on("tick", () => {
@@ -66,7 +87,6 @@ function Graph({ nodeData, linkData, handleNodeClick }) {
 
     // copy nodes into simulation
     simulation.nodes([...nodeData]);
-    
 
     // slow down with a small alpha
     simulation.alpha(0.02).restart();
@@ -75,24 +95,23 @@ function Graph({ nodeData, linkData, handleNodeClick }) {
     return () => simulation.stop();
   }, [nodeData]);
 
-  
+  useEffect(() => {
+    if (svg) {
+      const zoomBehavior = zoom()
+        .scaleExtent([0.5, 5]) // Set the minimum and maximum zoom level
+        .on("zoom", handleZoom);
 
-  // useEffect(() => {
-  //   const svg = d3.select(svgRef.current);
-  //   const zoomBehavior = zoom()
-  //     .scaleExtent([0.5, 5]) // Set the minimum and maximum zoom level
-  //     .on("zoom", handleZoom);
+      svg.call(zoomBehavior);
 
-  //   svg.call(zoomBehavior);
-    
-  //   function handleZoom(e) {
-  //     // Get the zoom transform
-  //     const transform = e.transform;
+      function handleZoom(e) {
+        // Get the zoom transform
+        const transform = e.transform;
 
-  //     // Apply the zoom transform to the elements you want to zoom
-  //     svg.selectAll("g").attr("transform", transform);
-  //   }
-  // }, [])
+        // Apply the zoom transform to the elements you want to zoom
+        svg.selectAll("g").attr("transform", transform);
+      }
+    }
+  }, [svg]);
 
   return (
     <>
@@ -103,41 +122,53 @@ function Graph({ nodeData, linkData, handleNodeClick }) {
         viewBox="-500 -500 1000 1000"
       >
         {animatedNodes.map((node) => (
-          <Node node={node} onClick={() => handleNodeClick(node)} focused={graph.origin.id === node.id} historical={graph.ancestors.includes(node)}/>
+          <Node
+            node={node}
+            onClick={() => handleNodeClick(node)}
+            focused={graph.origin.id === node.id}
+            historical={graph.ancestors.includes(node)}
+          />
         ))}
       </svg>
     </>
   );
 }
 
-function Node({node, onClick, focused, historical}) {
+function Node({ node, onClick, focused, historical }) {
+  const springs = useSpring({
+    config: {
+      mass: 5,
+      friction: 100,
+      tension: 300,
+    },
+    from: { opacity: 0, scale: 0 },
+    to: { opacity: 1, scale: 1 },
+  });
 
-    const springs = useSpring({
-        config: {
-            mass: 5,
-            friction: 100,
-            tension: 300,
-        },
-        from: { opacity: 0, scale: 0},
-        to: { opacity: 1, scale: 1},
-    })
+  const isFocused = useRef(false);
+  const isHistorical = useRef(false);
 
-    const isFocused = useRef(false);
-    const isHistorical = useRef(false);
+  useEffect(() => {
+    isFocused.current = focused;
+  }, [focused]);
 
-    useEffect(() => {
-        isFocused.current = focused;
-    }, [focused])
-    
-    useEffect(() => {
-      isHistorical.current = historical;
-  }, [focused])
+  useEffect(() => {
+    isHistorical.current = historical;
+  }, [focused]);
 
-    return (
-        <g key={node.id} onClick={onClick}>
-          <animated.text x={node.x} y={node.y} textAnchor={"middle"} fill={focused ? "blue" : (historical ? "green" : "black")}  style={springs}>{node.title}</animated.text>
-         </g>
-    )
+  return (
+    <g key={node.id} onClick={onClick}>
+      <animated.text
+        x={node.x}
+        y={node.y}
+        textAnchor={"middle"}
+        fill={focused ? "blue" : historical ? "green" : "black"}
+        style={springs}
+      >
+        {node.title}
+      </animated.text>
+    </g>
+  );
 }
 
 export default ForceGraph;
